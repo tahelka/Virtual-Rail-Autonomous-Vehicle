5f8e4a65ebce99d0b73ec4d4c3db4a7ac4496939
tahelka <73593127+tahelka@users.noreply.github.com>
2024-08-11 08:33:53 +0300

    real time statistics display

--- Control Application/src/pages/Statistics/Statistics.jsx
+++ Control Application/src/pages/Statistics/Statistics.jsx
@@ -1,5 +1,18 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+import WebSocketCharts from './WebSocketCharts';
+
 const Statistics = () => {
-  return <div>Statistics</div>;
+  return (
+    <div className="w-full min-w-[320px] overflow-x-auto">
+      <div className="max-w-7xl mx-auto px-4">
+        <Typography variant="h5" gutterBottom sx={{ marginBottom: "20px" }}>
+          Trip Statistics
+        </Typography>
+        <WebSocketCharts />
+      </div>
+    </div>
+  );
 };
 
 export default Statistics;
